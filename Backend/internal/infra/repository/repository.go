acd7197740873ed1cc1d552be37ebf81e72de5ae
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2022-12-05 12:12:28 -0300

    feat: repository

--- Backend/internal/infra/repository/repository.go
+++ Backend/internal/infra/repository/repository.go
@@ -1,12 +1,24 @@
 package repository
 
 import (
-    "cartola-backend/internal/infra/db"
-    "database/sql"
+	"errors"
+
+	"github.com/devfullcycle/imersao10-consolidacao/internal/infra/db"
 )
 
+var ErrQueriesNotSet = errors.New("queries not set")
+
 type Repository struct {
-    dbConn *sql.DB
-    *db.Queries
+	*db.Queries
 }
 
+func (r *Repository) SetQuery(q *db.Queries) {
+	r.Queries = q
+}
+
+func (r *Repository) Validade() error {
+	if r.Queries == nil {
+		return ErrQueriesNotSet
+	}
+	return nil
+}
